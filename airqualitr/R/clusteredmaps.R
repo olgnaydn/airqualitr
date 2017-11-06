d11ecd29de2a7b02f9ef090ef36b9cd22f45c64c
Olgun Aydin <Olgun_Aydin@epam.com>
2017-11-06 16:13:03 +0100

    removed clusteredmaps function. Instead of this, same abilities related to this function added to showstations

--- airqualitr/R/clusteredmaps.R
+++
@@ -1,8 +0,0 @@
-clusteredmaps<- function(city)
-  
-{
-  #clusteredmaps(city): This function builds map with bubbes for defined city.
-  #Bubble sizes are defined with related attribute value
-  
-}
-
