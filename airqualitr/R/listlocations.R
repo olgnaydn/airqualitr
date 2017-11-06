0ed68d2f9425176037dd71a122489f1b3e34301b
Olgun Aydin <Olgun_Aydin@epam.com>
2017-11-06 10:43:36 +0100

    Fixed problem and roxgenised package. Dependicies have been fixed

--- airqualitr/R/listlocations.R
+++ airqualitr/R/listlocations.R
@@ -1,6 +1,13 @@
 listlocations <- function()
-  
+
 {
-  
-  
+
+  #listlocations(city): Function is for listing air quality stations in the city defined by user.
+  #This functions returns available time intervals, last update date, name of the attributes, geolocations(lat-lon)
+  #for air quality stations related to city defined by user.
+  #getting Postgres Driver
+  drv <- dbDriver("PostgreSQL")
+  con <- dbConnect(drv, dbname = "development", host = "development.clgjf7569y1t.us-west-2.rds.amazonaws.com", port = 5432,
+                   user = "olgun", password = 'xxx')
+
 }
