9715002523217aa9a05690c3969df6eb1b1938d1
Olgun Aydin <Olgun_Aydin@epam.com>
2017-11-06 13:41:34 +0100

    minor fix

--- airqualitr/R/listlocations.R
+++ airqualitr/R/listlocations.R
@@ -3,9 +3,9 @@ listlocations <- function(city)
 {
 
   #listlocations(city): Function is for listing air quality stations in the city defined by user.
-  #This functions returns available time intervals, last update date, name of the attributes, geolocations(lat-lon)
+  #This functions returns, station names, name of the attributes, geolocations(lat-lon)
   #for air quality stations related to city defined by user.
-  
+
   #getting Postgres Driver
   drv <- dbDriver("PostgreSQL")
 
@@ -13,20 +13,20 @@ listlocations <- function(city)
   con <- dbConnect(drv, dbname = "development",
                    host = "airqualitr.clgjf7569y1t.us-west-2.rds.amazonaws.com",
                    port=5432, user = "airqualitr_team", password = "msgsu2017")
-  
+
   #defining query for getting stations according to user defined city
-  query_station_info<- paste("select distinct place, lat, long from airquality_data where place like", 
+  query_station_info<- paste("select distinct place, lat, long from airquality_data where place like",
                 "'",city,"%","'",
                 " group by place, lat, long",sep = "")
-  
+
   #getting stations for user defined city
   stations <- dbGetQuery(con,query_station_info)
-  
-  #getting attributes available for user defined city 
+
+  #getting attributes available for user defined city
   attributes <- dbGetQuery(con,"select * from airquality_data limit 1")
   available_attributes <- colnames(attributes)[4:27]
 
   list("station_info",stations,"available_attributes",available_attributes )
-                         
+
 
 }
