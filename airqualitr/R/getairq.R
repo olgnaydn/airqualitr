0de1bd894a8535d30da3543b7bfe2ae32fed90e0
Olgun Aydin <Olgun_Aydin@epam.com>
2017-11-06 16:43:42 +0100

    generated getairq function

--- airqualitr/R/getairq.R
+++ airqualitr/R/getairq.R
@@ -1,6 +1,23 @@
-getairq <- function()
+getairq <- function(city, variables)
 
 {
 
+#getairq(attributes,lat,lon, since, to): Function is for getting airquality data related to
+#location  defined by user
+#This function returns air quality data related to user defined variables.
+
+  drv <- dbDriver("PostgreSQL")
+
+  #connectiong server
+  con <- dbConnect(drv, dbname = "development",
+                   host = "airqualitr.clgjf7569y1t.us-west-2.rds.amazonaws.com",
+                   port=5432, user = "airqualitr_team", password = "msgsu2017")
+
+  #defining query for getting attributes defined by user, according to user defined city
+  query_stations<- paste('select place, date, lat, long, ', variables,' from airquality_data',sep = "")
+
+  #getting information for all stations of defined city
+  values <- dbGetQuery(con,query_stations)
+  values_for_defined_city <- values[grep(city, values$place),]
 
 }
