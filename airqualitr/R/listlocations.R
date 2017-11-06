3c3d7f9888a56b41ab59ed34010a725263907d40
Olgun Aydin <Olgun_Aydin@epam.com>
2017-11-06 11:46:27 +0100

    added query getting station names and geolocation information for city defined by user

--- airqualitr/R/listlocations.R
+++ airqualitr/R/listlocations.R
@@ -1,13 +1,28 @@
-listlocations <- function()
+listlocations <- function(city)
 
 {
 
   #listlocations(city): Function is for listing air quality stations in the city defined by user.
   #This functions returns available time intervals, last update date, name of the attributes, geolocations(lat-lon)
   #for air quality stations related to city defined by user.
+  
   #getting Postgres Driver
   drv <- dbDriver("PostgreSQL")
-  con <- dbConnect(drv, dbname = "development", host = "development.clgjf7569y1t.us-west-2.rds.amazonaws.com", port = 5432,
-                   user = "olgun", password = 'xxx')
+
+  #connectiong server
+  con <- dbConnect(drv, dbname = "development",
+                   host = "airqualitr.clgjf7569y1t.us-west-2.rds.amazonaws.com",
+                   port=5432, user = "airqualitr_team", password = "msgsu2017")
+  
+  #defining query for getting stations according to user defined city
+  query<- paste("select distinct place, lat, long from airquality_data where place like", 
+                "'",city,"%","'",
+                " group by place, lat, long",sep = "")
+  
+  #getting stations for user defined city
+  stations <- dbGetQuery(con,query)
+
+  list(stations)
+                         
 
 }
